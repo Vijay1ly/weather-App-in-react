531500ec2c293df080f0a0dedfc565c448fef88e
Vijay1ly <93240129+Vijay1ly@users.noreply.github.com>
2021-12-21 16:10:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+
+const api = {
+  key: "4ed11f2ea13a1cb52e1f9df066b18208",
+  base:"https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  const dateBuild = (d) => {
+    let date = String(new window.Date());
+    date = date.slice(3, 15);
+    return date;
+  };
+
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery("");
+          setWeather(result);
+          console.log(result);
+        });
+    }
+  };
+
+  return (
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 18
+            ? "App hot"
+            : "App cold"
+          : "App"
+      }
+    > 
+    <h1 data-text="weatherapp">weatherapp</h1>
+      <main>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search-bar"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main != "undefined"  ? (
+          <div>
+            <div className="location-container">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date"> {dateBuild(new Date())}</div>
+            </div>
+            <div className="weather-container">
+              <div className="temperature">
+                {Math.round(weather.main.temp)}°C
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+           <div className="date"> {dateBuild(new Date())}</div>
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default App;
